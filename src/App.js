8cbb82b14b5c392ba8b3eba18bd90d1dc3b7ba88
sayhan2021 <schowdhury0250@richmondhillhs.org>
2021-02-11 04:02:54 +0000

    street

--- src/App.js
+++ src/App.js
@@ -9,7 +9,30 @@ function App() {
       <Navbar/>
       <h1 className="header">Earn More, Pay Less? Average Housing Costs and Incomes by NYC Borough</h1>
 
-      <DataView/>
+      <DataView
+       borough="Staten Island"
+       renterCost="$14,292"
+       ownerCost="$28,752"/>
+
+             <DataView
+       borough="Queens"
+       renterCost="$16,812"
+       ownerCost="$29,256"/>
+
+             <DataView
+       borough="Manhattan"
+       renterCost="$18,900"
+       ownerCost="$36,252"/>
+
+             <DataView
+       borough="Brooklyn"
+       renterCost="$14,144"
+       ownerCost="$31,908"/>
+
+             <DataView
+       borough="Bronx"
+       renterCost="$13,176"
+       ownerCost="$28,548"/>
     </div>
   );
 }
